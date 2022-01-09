19ebef2085422136d3d0beea070c7d67e8b769bd
xiaoyuxiu <1917429455@qq.com>
2022-01-09 21:04:44 +0800

    Site updated: 2022-01-09 21:04:43

--- js/main.js
+++ js/main.js
@@ -40,7 +40,9 @@ require([], function (){
 		          "nb_sides": 5
 		        },
 		        "image": {
-		          "src": "img/github.svg",
+		
+ 		         "src": "img/2.jpg", 
+		          //"src": "img/github.svg", 
 		          "width": 100,
 		          "height": 100
 		        }
